f63a57a41504599f1af1833263e3ba3c1a09ca4b
ico12319 <hristokubev@gmail.com>
2024-07-15 17:34:22 +0300

    fix description

--- 191. Number of 1 Bits/main.cpp
+++ 191. Number of 1 Bits/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
-// 231. Power of Two
-// Given an integer n, return true if it is a power of two. Otherwise, return false.
+// 191. Number of 1 Bits
+// Write a function that takes the binary representation of a positive integer and returns the number of
+//set bits it has (also known as the Hamming weight).
 
 
-int hammingWeight(int number){
+int hammingWeight(int number){ // 0 ms
     int count = 0;
     int mask = 1;
     for(int i = 0;i<32;i++){
