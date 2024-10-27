c4ed5fae5e061153aeaf65896f09fb39306cccd0
ico12319 <hristokubev@gmail.com>
2024-10-27 12:40:48 +0200

    add optimised linear solution

--- 1460. Make Two Arrays Equal by Reversing Subarrays/main.cpp
+++ 1460. Make Two Arrays Equal by Reversing Subarrays/main.cpp
@@ -1,18 +1,34 @@
 #include <iostream>
 
-bool canBeEqual(std::vector<int>& target, std::vector<int>& arr) {
-    const size_t size1 = target.size();
-    const size_t size2 = arr.size();
-    if(size1!=size2)
+//1460. Make Two Arrays Equal by Reversing Subarrays
+//You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty subarray of arr and reverse it. You are allowed to make any number of steps.Return true if you can make arr equal to target or false otherwise.
+
+constexpr size_t MIN_VALUE = 1;
+constexpr size_t MAX_VALUE = 1000;
+std::vector<int> countingSort(std::vector<int>& arr){
+    std::vector<int> count(MAX_VALUE - MIN_VALUE + 1,0);
+    for(int i = 0;i<arr.size();i++)
+        count[arr[i] - MIN_VALUE]++;
+    for(int i = 1;i<MAX_VALUE-MIN_VALUE+1;i++)
+        count[i] += count[i - 1];
+    std::vector<int> res(MAX_VALUE - MIN_VALUE + 1,0);
+    for(int i = 0;i<arr.size();i++){
+        size_t correctIndex = --count[arr[i] - MIN_VALUE];
+        res[correctIndex] = arr[i];
+    }
+    return res;
+}
+
+bool canBeEqual(std::vector<int>& target,std::vector<int>& arr){
+    if(target.size() != arr.size())
         return false;
-    
-    std::sort(target.begin(),target.end());
-    std::sort(arr.begin(),arr.end());
+    target = countingSort(target);
+    arr = countingSort(arr);
     return target == arr;
 }
 
 int main(int argc, const char * argv[]) {
     std::vector<int> target = {1,2,3,4};
-    std::vector<int> arr = {2,4,1};
+    std::vector<int> arr = {2,4,1,3};
     std::cout<<canBeEqual(target, arr)<<std::endl;
 }
