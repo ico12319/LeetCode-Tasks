d5f7e802f97694c60601d8b5163051e15a0d6e5a
ico12319 <hristokubev@gmail.com>
2024-10-27 12:59:03 +0200

    add solution

--- 3194. Minimum Average of Smallest and Largest Elements/main.cpp
+++ 3194. Minimum Average of Smallest and Largest Elements/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
 //3194. Minimum Average of Smallest and Largest Elements
-//You have an array of floating point numbers averages which is initially empty. You are given an array nums of n integers where n is even.You repeat the following procedure n / 2 times: Remove the smallest element, minElement, and the largest element maxElement, from nums.Add (minElement + maxElement) / 2 to averages.Return the minimum element in averages.
+//You have an array of floating point numbers averages which is initially empty. You are given an array nums of n integers where n is even.You repeat the following procedure n / 2 times:
 
-double minimumAverage(std::vector<int>& nums) {
-    const size_t size = nums.size();
+double minimumAverage(std::vector<int>& nums){
     std::sort(nums.begin(),nums.end());
-    int leftPointer = 0;
-    int rightPointer = nums.size() - 1;
-    double minAverage = __DBL_MAX__;
-    while(leftPointer < rightPointer){
-        auto average = (nums[leftPointer] + nums[rightPointer]) / 2.0;
-        minAverage = std::min(minAverage,average);
-        leftPointer++;
-        rightPointer--;
+    int left = 0;
+    double minAv = INT_MAX;
+    int right = nums.size() - 1;
+    while(left < right){
+        double difference = (nums[left] + nums[right]) / 2.0;
+        minAv = std::min(minAv,difference);
+        left++;
+        right--;
     }
-    return minAverage;
+    return minAv;
 }
 
 int main(int argc, const char * argv[]) {
-    std::vector<int> nums = {7,8,3,4,15,13,4,1};
-    std::cout<<minimumAverage(nums)<<std::endl;
+    std::vector<int> v = {7,8,3,4,15,13,4,1};
+    std::cout<<minimumAverage(v)<<std::endl;
 }
