f54864c421789a92128485fef2e8107eae5fb4af
ico12319 <hristokubev@gmail.com>
2024-12-15 17:46:31 +0200

    add optimised solution using sliding window approach

--- 3. Longest Substring Without Repeating Characters/main.cpp
+++ 3. Longest Substring Without Repeating Characters/main.cpp
@@ -1,32 +1,32 @@
-#include <stdio.h>
 #include <iostream>
 #include <unordered_set>
 
 //3. Longest Substring Without Repeating Characters
-// Given a string s, find the length of the longest substring without repeating characters.
+//Given a string s, find the length of the longestsubstring without repeating characters.
 
-int lengthOfLongestSubstring(std::string& s){
-    int start = 0;
-    int index = 0;
-    int max = 0;
-    int size = s.length();
-    std::unordered_set<char> unique;
-    while(index < size){
-        if(unique.contains(s[index])){
-            unique.erase(s[start]);
-            start++;
-            continue;
+size_t length_of_the_longest_substring(const std::string& s){
+    std::unordered_set<char> set;
+    size_t left_ptr = 0;
+    size_t right_ptr = 0;
+    size_t max_size = 0;
+    while(true){
+        size_t curr_window_size = (right_ptr - left_ptr) + 1;
+        if(!set.contains(s[right_ptr])){
+            set.insert(s[right_ptr]);
+            right_ptr++;
+            max_size = std::max(max_size,curr_window_size);
+            if(right_ptr >= s.size()) break;
         }
-        unique.insert(s[index]);
-        int currSize = unique.size();
-        if(currSize > max)
-            max = currSize;
-        index++;
+        else{
+            set.erase(s[left_ptr]);
+            left_ptr++;
+        }
     }
-    return max;
+    return max_size;
 }
 
-int main(){
-    std::string str = "pwwkew";
-    std::cout<<lengthOfLongestSubstring(str)<<std::endl;
+int main(int argc, const char * argv[]) {
+    std::string s = "xxxx";
+    std::cout<<length_of_the_longest_substring(s)<<std::endl;
 }
+
