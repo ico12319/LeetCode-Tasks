9c3f40261af45319286fc6c16f0dc144a4e6bdcb
Hristo Partenov <hristokubev@gmail.com>
2025-05-06 00:29:54 +0300

    add sql solution

---
+++ 178. Rank Scores.sql
@@ -0,0 +1,13 @@
+/*178. Rank Scores
+Write a solution to find the rank of the scores. The ranking should be calculated according to the following rules:
+
+The scores should be ranked from the highest to the lowest.
+If there is a tie between two scores, both should have the same ranking.
+After a tie, the next ranking number should be the next consecutive integer value. In other words, there should be no holes between ranks.
+Return the result table ordered by score in descending order.
+*/
+
+SELECT id, DENSE_RANK() OVER(ORDER BY score DESC) AS rank FROM Scores;
+
+
+
